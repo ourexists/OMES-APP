d91122c2c7ea788bc81fa7eee8ea5f076e55ee4d
pengcheng <pengcheng@wondersgroup.com>
2026-01-09 17:18:40 +0800

    场景选择问题修复

--- core/store/index.ts
+++ core/store/index.ts
@@ -1,17 +1,21 @@
 import {Dict, dict} from "./dict";
 import {User, user} from "./user";
+import {WorkshopTree, workshopTree} from "./workshopTree";
 
 type Store = {
     user: User;
     dict: Dict;
+    workshopTree: WorkshopTree;
 };
 
 export function useStore(): Store {
     return {
         user,
-        dict
+        dict,
+        workshopTree
     };
 }
 
 export * from "./dict";
 export * from "./user";
+export * from "./workshopTree";
