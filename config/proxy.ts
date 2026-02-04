5514ced4bf93f71d2b806f79fa4a688db4a3dd40
pengcheng <pengcheng@wondersgroup.com>
2026-02-04 10:31:23 +0800

    1.组件生命周期 2.配置

--- config/proxy.ts
+++ config/proxy.ts
@@ -12,7 +12,7 @@ export const proxy = {
     // 生产环境配置
     prod: {
         // 官方测试地址
-        target: "http://22x2097c96.imwork.net",
+        target: "https://222qc09hk796.vicp.fun",
         changeOrigin: true,
         rewrite: (path: string) => path.replace("/prod", "/api")
     }
