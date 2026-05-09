e37101e9597580ff5050bd0d5046dba9da845967
pengcheng <pengcheng@wondersgroup.com>
2026-05-09 14:50:26 +0800

    1.兼容处理

--- config/proxy.ts
+++ config/proxy.ts
@@ -3,7 +3,7 @@ export const proxy = {
     dev: {
         // 官方测试地址
         // 本地地址
-        target: "http://127.0.0.1:10010",
+        target: "https://www.tcwt.net:9400",
         changeOrigin: true,
         // target: "http://22x2097c96.imwork.net",
         rewrite: (path: string) => path.replace("/dev", "")
@@ -12,7 +12,7 @@ export const proxy = {
     // 生产环境配置
     prod: {
         // 官方测试地址
-        target: "https://222qc09hk796.vicp.fun",
+        target: "https://www.tcwt.net:9400",
         changeOrigin: true,
         rewrite: (path: string) => path.replace("/prod", "/api")
     }
